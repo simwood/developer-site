fe619c9bda4c32eaf65b89edd9707ce32c1ca648
David Maitland <david.maitland@sipcentric.com>
2020-09-29 16:07:05 +0100

    Disable search until we get our algolia API key

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,11 +83,11 @@ module.exports = {
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Simwood eSMS Limited.`,
     },
-    algolia: {
-      apiKey: 'YOUR_API_KEY',
-      indexName: 'YOUR_INDEX_NAME',
-      searchParameters: {}, // Optional (if provided by Algolia)
-    },
+    // algolia: {
+    //   apiKey: 'YOUR_API_KEY',
+    //   indexName: 'YOUR_INDEX_NAME',
+    //   searchParameters: {}, // Optional (if provided by Algolia)
+    // },
   },
   presets: [
     [
