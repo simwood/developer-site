d1a50a99390946f0acea1885cb9247483330629d
David Maitland <david.maitland@sipcentric.com>
2020-09-29 16:12:33 +0100

    Added some placeholder content for the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,32 +8,31 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use</>,
+    title: <>Introducing Simwood</>,
     imageUrl: 'img/1.svg',
     description: (
       <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Fusce tempor enim in elementum hendrerit. Lorem ipsum dolor sit amet, consectetur.
+        We are a dynamic wholesale telecoms carrier specialising in innovation-driven wholesale VoIP telephony. Our smart, feature-rich hosted voice platform enables our partners to deliver an industry-leading service to their customers.
+        <br /><br />
+        With a wholly owned and operated network, a team of highly specialised engineers, ground-breaking fraud protection and an uncompromisingly honest approach, we are a rare find in the telecom industry.
       </>
     ),
   },
   {
-    title: <>Focus on What Matters</>,
+    title: <>A rock solid network</>,
     imageUrl: 'img/2.svg',
     description: (
       <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Fusce tempor enim in elementum hendrerit. Lorem ipsum dolor sit amet, consectetur.
+        Our highly advanced and ever-evolving telephony network has been built from the ground up by our team of exceptional engineers. By controlling every element of our infrastructure, we can ensure your calls get the priority they deserve. There are no delays from supply chains, no blaming ‘service providers’; we own our network and take pride in operating it so your service won’t skip a beat. Underpinned by ground-breaking technology, our network is constantly being updated and advanced so it never ceases to add value to our customers.
       </>
     ),
   },
   {
-    title: <>Powered by Simwood</>,
+    title: <>Built by real engineers</>,
     imageUrl: 'img/3.svg',
     description: (
       <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Fusce tempor enim in elementum hendrerit. Lorem ipsum dolor sit amet, consectetur.
+        For over 20 years we have been pioneers in the industry, innovating the latest technological developments and shaping future technologies. Our position at the heart of the communities we belong to, and our thought-leadership, mean we punch dramatically above our weight in international circles. Our customers benefit from the very best that technology has to offer because we remain five steps ahead of the herd. Great pains are taken to own and operate all equipment and network hardware, where it adds value to do so. This means our engineers have access to every element of your service, without having to rely on a less experienced third party.
       </>
     ),
   },
