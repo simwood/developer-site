3c35d3326f70d8838f74e153fe186eab5fa8a536
David Maitland <david.maitland@sipcentric.com>
2020-09-30 16:22:35 +0100

    Updated to alpha.64

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,7 +95,6 @@ module.exports = {
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'index',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
